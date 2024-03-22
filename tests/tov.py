58cdcc330239e46879d54210f9edaf0c7080a35e
Peter T. H. Pang <tsunhopang@gmail.com>
2024-03-22 11:24:08 +0100

    Update test

--- tests/tov.py
+++ tests/tov.py
@@ -20,5 +20,12 @@ def test_tov():
         micro_eos['p'],
         micro_eos['e']
     )
+    eos_tuple = (
+        eos_input.n,
+        eos_input.p,
+        eos_input.h,
+        eos_input.e,
+        eos_input.dloge_dlogp
+    )
     # solve the TOV equation for M-R-Lambda curve
-    logpcs, ms, rs, Lambdas = eos.construct_family(eos_input)
+    logpcs, ms, rs, Lambdas = eos.construct_family(eos_tuple)
